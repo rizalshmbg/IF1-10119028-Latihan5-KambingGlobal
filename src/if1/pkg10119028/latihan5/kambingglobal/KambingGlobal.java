af9faa2c11cfb07b9719787ac1169e93bc030286
Rizal Sihombing <66808677+rizalshmbg@users.noreply.github.com>
2020-10-18 01:08:56 +0700

    Add files via upload

---
+++ src/if1/pkg10119028/latihan5/kambingglobal/KambingGlobal.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10119028.latihan5.kambingglobal;
+
+/**
+ *
+ * @author
+ * NAMA     : Rizal Sihombing
+ * KELAS    : IF1
+ * NIM      : 10119028
+ * Deskripsi Program : Program ini berisi program untuk
+ */
+public class KambingGlobal {
+    int jumlahKambing = 88;
+    
+    // Method untuk menampilkan jumlah kambing
+    public void getJumlahKambing() {
+        System.out.println("Jumlah kambing: " + jumlahKambing);
+    }
+    
+    public void tambahKambing() {
+        jumlahKambing = jumlahKambing + 5;
+        System.out.println("Jumlah kambing setelah ditambah: " + jumlahKambing);
+    }
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        KambingGlobal kambingSusu = new KambingGlobal();
+        
+        // Menampilkan jumlah kambing yang ada pada saat program pertama x berjalan
+        kambingSusu.getJumlahKambing();
+        
+        // Menambah satu kambing
+        kambingSusu.tambahKambing();
+        
+        // Menampilkan jumlah kambing yang ada
+        kambingSusu.getJumlahKambing();
+    }
+    
+}
